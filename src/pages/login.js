c2c117f3f4d4bda0e907b3bdb8c65ede65a08e9f
João Miguel Moscardini <joaomiguelmv_04@hotmail.com>
2023-04-10 20:31:48 -0300

    tela de login

---
+++ src/pages/login.js
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import { View, Text, TextInput, TouchableOpacity, StyleSheet } from "react-native";
+
+const Login = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleLogin = () => {
+        // Implementar lógica de login
+        console.log(`Email: ${email} - Password: ${password}`);
+    }
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>Login</Text>
+            <TextInput
+                style={styles.input}
+                placeholder="E-mail"
+                value={email}
+                onChangeText={setEmail}
+            />
+            <TextInput
+                style={styles.input}
+                placeholder="Senha"
+                value={password}
+                secureTextEntry={true}
+                onChangeText={setPassword}
+            />
+            <TouchableOpacity style={styles.button} onPress={handleLogin}>
+                <Text style={styles.buttonText}>Entrar</Text>
+            </TouchableOpacity>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#FFF"
+    },
+    title: {
+        fontSize: 20,
+        textAlign: "center",
+        margin: 10
+    },
+    input: {
+        width: '80%',
+        borderColor: "#CCC",
+        borderWidth: 1,
+        borderRadius: 4,
+        padding: 10,
+        marginVertical: 10
+    },
+    button: {
+        width: "80%",
+        padding: 10,
+        backgroundColor: "#000",
+        borderRadius: 4,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    buttonText: {
+        color: "#FFF",
+        fontWeight: "bold"
+    }
+});
+
+export default Login;
---
+++ src/routes.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Main from "./pages/main";
+import Login from "./pages/login";
+
+const Stack = createStackNavigator();
+
+export default function Routes() {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="Login" component={Login}
+                    options={{
+                        title: "LOGIN",
+                        headerTitleAlign: "center",
+                        headerStyle: {
+                            backgroundColor: "#8c018c",
+                        },
+                        headerTitleStyle: {
+                            fontWeight: "bold",
+                            color: "#FFF"
+                        }
+                    }} />
+                <Stack.Screen name="Main" component={Main}
+                    options={{
+                        title: "GitHub Viewer",
+                        headerTitleAlign: "center",
+                        headerStyle: {
+                            backgroundColor: "#8c018c",
+                        },
+                        headerTitleStyle: {
+                            fontWeight: "bold",
+                            color: "#FFF"
+                        }
+                    }} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+}
