0a0467d3a378bb8cc85b1faabee7f8dc871a315a
João Miguel Moscardini <joaomiguelmv_04@hotmail.com>
2023-05-02 20:30:11 -0300

    deletar e ordenar usuarios

--- src/pages/main.js
+++ src/pages/main.js
@@ -6,84 +6,101 @@ import { Container, Form, Input, SubmitButton, List, User, Name, Avatar, Bio, Pr
 import api from "../services/api";
 
 export default class Main extends Component {
-    state = {
-        newUser: "",
-        users: [],
+  state = {
+    newUser: "",
+    users: [],
+    loading: false
+  }
+
+  async componentDidMount() { // busca os dados do storage
+    const users = await AsyncStorage.getItem('users')
+    if (users) {
+      this.setState({ users: JSON.parse(users) })
+    }
+  }
+
+  async componentDidUpdate(_, prevState) { // salva os dados no storage 
+    const { users } = this.state;
+
+    if (prevState.users !== users) {
+      await AsyncStorage.setItem('users', JSON.stringify(users))
+    }
+  }
+
+  handleAddUser = async () => {
+    try {
+      const { users, newUser } = this.state
+      this.setState({ loading: true })
+      const response = await api.get(`/users/${newUser}`)
+
+
+      const data = {
+        name: response.data.name,
+        login: response.data.login,
+        bio: response.data.bio,
+        avatar: response.data.avatar_url
+      }
+
+      this.setState({
+        users: [data, ...users],
+        newUser: '',
         loading: false
+      })
+      Keyboard.dismiss()
+    } catch (err) {
+      console.log(err)
+      alert('Usuário não encontrado')
+      this.setState({ loading: false })
     }
 
-    async componentDidMount() { // busca os dados do storage
-        const users = await AsyncStorage.getItem('users')
-        if (users) {
-            this.setState({ users: JSON.parse(users) })
-        }
-    }
+  }
 
-    async componentDidUpdate(_, prevState) { // salva os dados no storage 
-        const { users } = this.state;
+  render() {
+    const { newUser, users, loading } = this.state;
+    return (
+      <Container>
+        <Form>
+          <Input
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Adicionar usuário"
+            value={newUser}
+            onChangeText={text => this.setState({ newUser: text })}
+            returnKeyType="send"
+            onSubmitEditing={this.handleAddUser}
+          />
 
-        if (prevState.users !== users) {
-            await AsyncStorage.setItem('users', JSON.stringify(users))
-        }
-    }
+          <SubmitButton loading={loading} onPress={this.handleAddUser}>
+            {loading ? (<ActivityIndicator color='#FFF' />) : <Icon name="add" size={20} color="#FFF" />}
+          </SubmitButton>
+        </Form>
 
-    handleAddUser = async () => {
-        const { users, newUser } = this.state
-        this.setState({ loading: true })
-        const response = await api.get(`/users/${newUser}`)
+        <List
+          showsVerticalScrollIndicator={false}
+          data={users}
+          keyExtractor={user => user.login}
+          renderItem={({ item }) => (
+            <User>
+              <Avatar source={{ uri: item.avatar }} />
+              <Name>{item.name}</Name>
+              <Bio>{item.bio}</Bio>
+              <ProfileButton onPress={() => {
+                this.props.navigation.navigate('user', { user: item })
 
-
-        const data = {
-            name: response.data.name,
-            login: response.data.login,
-            bio: response.data.bio,
-            avatar: response.data.avatar_url
-        }
-
-        this.setState({
-            users: [...users, data],
-            newUser: '',
-            loading: false
-        })
-        Keyboard.dismiss()
-    }
-
-    render() {
-        const { newUser, users, loading } = this.state;
-        return (
-            <Container>
-                <Form>
-                    <Input
-                        autoCorrect={false}
-                        autoCapitalize="none"
-                        placeholder="Adicionar usuário"
-                        value={newUser}
-                        onChangeText={text => this.setState({ newUser: text })}
-                        returnKeyType="send"
-                        onSubmitEditing={this.handleAddUser}
-                    />
-
-                    <SubmitButton loading={loading} onPress={this.handleAddUser}>
-                        {loading ? (<ActivityIndicator color='#FFF' />) : <Icon name="add" size={20} color="#FFF" />}
-                    </SubmitButton>
-                </Form>
-
-                <List
-                    showsVerticalScrollIndicator={false}
-                    data={users}
-                    keyExtractor={user => user.login}
-                    renderItem={({ item }) => (
-                        <User>
-                            <Avatar source={{ uri: item.avatar }} />
-                            <Name>{item.name}</Name>
-                            <Bio>{item.bio}</Bio>
-                            <ProfileButton onPress={() => { }}>
-                                <ProfileButtonText>Ver perfil</ProfileButtonText>
-                            </ProfileButton>
-                        </User>
-                    )}
-                />
-            </Container>
-        );
-    }
+              }}>
+                <ProfileButtonText>Ver perfil</ProfileButtonText>
+              </ProfileButton>
+              <ProfileButton onPress={() => {
+                this.setState({ users: users.filter(user => user.login !== item.login) })
+              }}
+                style={{ backgroundColor: '#454443' }}
+              >
+                <ProfileButtonText>Excluir</ProfileButtonText>
+              </ProfileButton>
+            </User>
+          )}
+        />
+      </Container>
+    );
+  }
 }
--- src/routes.js
+++ src/routes.js
@@ -3,39 +3,53 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import Main from "./pages/main";
 import Login from "./pages/login";
+import User from "./pages/user";
 
 const Stack = createStackNavigator();
 
 export default function Routes() {
-    return (
-        <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen name="Login" component={Login}
-                    options={{
-                        title: "LOGIN",
-                        headerTitleAlign: "center",
-                        headerStyle: {
-                            backgroundColor: "#8c018c",
-                        },
-                        headerTitleStyle: {
-                            fontWeight: "bold",
-                            color: "#FFF"
-                        }
-                    }} />
-                <Stack.Screen name="Main" component={Main}
-                    options={{
-                        title: "GitHub Viewer",
-                        headerTitleAlign: "center",
-                        headerLeft: null,
-                        headerStyle: {
-                            backgroundColor: "#8c018c",
-                        },
-                        headerTitleStyle: {
-                            fontWeight: "bold",
-                            color: "#FFF"
-                        }
-                    }} />
-            </Stack.Navigator>
-        </NavigationContainer>
-    );
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={Login}
+          options={{
+            title: "LOGIN",
+            headerTitleAlign: "center",
+            headerStyle: {
+              backgroundColor: "#8c018c",
+            },
+            headerTitleStyle: {
+              fontWeight: "bold",
+              color: "#FFF"
+            }
+          }} />
+        <Stack.Screen name="Main" component={Main}
+          options={{
+            title: "GitHub Viewer",
+            headerTitleAlign: "center",
+            headerLeft: null,
+            headerStyle: {
+              backgroundColor: "#8c018c",
+            },
+            headerTitleStyle: {
+              fontWeight: "bold",
+              color: "#FFF"
+            }
+          }} />
+        <Stack.Screen name="user" component={User}
+          options={{
+            title: "Perfil do Usuário",
+            headerTitleAlign: "center",
+            headerTintColor: "#FFF",
+            headerStyle: {
+              backgroundColor: "#8c018c",
+            },
+            headerTitleStyle: {
+              fontWeight: "bold",
+              color: "#FFF"
+            }
+          }} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
